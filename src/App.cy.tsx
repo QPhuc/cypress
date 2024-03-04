ba46f0ff76c0669c639aefbd30706eb77ad4c2a7
Phuc Tran <tqp1610@gmail.com>
2024-03-04 22:06:05 +0700

    multi result

--- src/App.cy.tsx
+++ src/App.cy.tsx
@@ -2,8 +2,20 @@ import React from 'react'
 import App from './App'
 
 describe('<App />', () => {
+  befor
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
     cy.mount(<App />)
+
+    // cy.get("table:first > thead > tr:first > th").should(($cols) => {
+    //   expect($cols).to.have.length(7);
+    //   expect($cols[0]).to.have.text("Report Name");
+    //   expect($cols[1]).to.have.text("Product Line");
+    //   expect($cols[2]).to.have.text("Solution Family");
+    //   expect($cols[3]).to.have.text("Report Group");
+    //   expect($cols[4]).to.have.text("Latest Refresh Date/Time");
+    //   expect($cols[5]).to.have.text("Last Published Date/Time");
+    //   expect($cols[6]).to.have.text("Report Author");
+    // });
   })
 })
